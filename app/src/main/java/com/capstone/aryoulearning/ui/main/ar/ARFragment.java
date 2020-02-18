6c051807a26ca34a1ad277692963d33954fbdadc
hyunki <dhchung317@gmail.com>
2020-02-18 02:17:49 -0500

    starts refactor of data structure variables and renderable parse methods

--- app/src/main/java/com/capstone/aryoulearning/ui/main/ar/ARFragment.java
+++ app/src/main/java/com/capstone/aryoulearning/ui/main/ar/ARFragment.java
@@ -1,6 +1,7 @@
 package com.capstone.aryoulearning.ui.main.ar;
 
 import android.content.Context;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.GestureDetector;
 import android.view.LayoutInflater;
@@ -20,11 +21,16 @@ import com.capstone.aryoulearning.ui.main.MainViewModel;
 import com.capstone.aryoulearning.ui.main.controller.NavListener;
 import com.capstone.aryoulearning.util.audio.PronunciationUtil;
 import com.capstone.aryoulearning.viewmodel.ViewModelProviderFactory;
+import com.google.ar.sceneform.rendering.ModelRenderable;
 import com.google.ar.sceneform.ux.ArFragment;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 import javax.inject.Inject;
 
@@ -41,14 +47,19 @@ public class ARFragment extends DaggerFragment {
 
     private MainViewModel mainViewModel;
 
-    private List<Model> modelList = new ArrayList<>();
-
     private static final int RC_PERMISSIONS = 0x123;
     public static final String MODEL_LIST = "MODEL_LIST";
+
     private NavListener listener;
 
     private GestureDetector gestureDetector;
     private ArFragment arFragment;
+
+    private List<Model> modelList = new ArrayList<>();
+    private List<HashMap<String, CompletableFuture<ModelRenderable>>> futureModelMapList = new ArrayList<>();
+    private HashMap<String, CompletableFuture<ModelRenderable>> futureLetterMap = new HashMap<>();
+    private List<HashMap<String, ModelRenderable>> modelMapList = new ArrayList<>();
+    private HashMap<String, ModelRenderable> letterMap = new HashMap<>();
 
     @Override
     public void onAttach(Context context) {
@@ -88,6 +99,12 @@ public class ARFragment extends DaggerFragment {
             @Override
             public void onChanged(List<Model> models) {
                 modelList = models;
+
+                setListMapsOfFutureModels(modelList);
+                setMapOfFutureLetters(futureModelMapList);
+
+                setModelRenderables(futureModelMapList);
+                setLetterRenderables(futureLetterMap);
             }
         });
 
@@ -98,5 +115,78 @@ public class ARFragment extends DaggerFragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+    }
+
+
+    private void setListMapsOfFutureModels(List<Model> modelList) {
+
+        for (int i = 0; i < modelList.size(); i++) {
+            HashMap<String, CompletableFuture<ModelRenderable>> futureMap = new HashMap();
+            futureMap.put(modelList.get(i).getName(),
+                    ModelRenderable.builder().
+                            setSource(getActivity(), Uri.parse(modelList.get(i).getName() + ".sfb")).build());
+            futureModelMapList.add(futureMap);
+        }
+    }
+
+    private void setMapOfFutureLetters(List<HashMap<String, CompletableFuture<ModelRenderable>>> futureMapList) {
+        for (int i = 0; i < futureMapList.size(); i++) {
+            String modelName = futureMapList.get(i).keySet().toString();
+            for (int j = 0; j < modelName.length(); j++) {
+                futureLetterMap.put(Character.toString(modelName.charAt(j)), ModelRenderable.builder().
+                        setSource(getActivity(), Uri.parse(modelName.charAt(j) + ".sfb")).build());
+            }
+        }
+    }
+
+    private void setModelRenderables(List<HashMap<String, CompletableFuture<ModelRenderable>>> futureModelMapList) {
+
+        for (int i = 0; i < futureModelMapList.size(); i++) {
+
+            for (Map.Entry<String, CompletableFuture<ModelRenderable>> e : futureModelMapList.get(i).entrySet()) {
+
+                HashMap<String, ModelRenderable> modelMap = new HashMap<>();
+
+                CompletableFuture.allOf(e.getValue())
+                        .handle(
+                                (notUsed, throwable) -> {
+                                    // When you build a Renderable, Sceneform loads its resources in the background while
+                                    // returning a CompletableFuture. Call handle(), thenAccept(), or check isDone()
+                                    // before calling get().
+                                    if (throwable != null) {
+                                        return null;
+                                    }
+                                    try {
+                                        modelMap.put(e.getKey(), e.getValue().get());
+                                    } catch (InterruptedException | ExecutionException ex) {
+                                    }
+                                    return null;
+                                });
+                modelMapList.add(modelMap);
+            }
+        }
+//        hasFinishedLoadingModels = true;
+    }
+
+    private void setLetterRenderables(HashMap<String, CompletableFuture<ModelRenderable>> futureLetterMap) {
+        for (Map.Entry<String, CompletableFuture<ModelRenderable>> e : futureLetterMap.entrySet()) {
+
+            CompletableFuture.allOf(e.getValue())
+                    .handle(
+                            (notUsed, throwable) -> {
+                                // When you build a Renderable, Sceneform loads its resources in the background while
+                                // returning a CompletableFuture. Call handle(), thenAccept(), or check isDone()
+                                // before calling get().
+                                if (throwable != null) {
+                                    return null;
+                                }
+                                try {
+                                    letterMap.put(e.getKey(), e.getValue().get());
+                                } catch (InterruptedException | ExecutionException ex) {
+                                }
+                                return null;
+                            });
+        }
+//        hasFinishedLoadingLetters = true;
     }
 }
